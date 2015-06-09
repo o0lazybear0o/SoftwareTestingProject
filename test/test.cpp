fcab3348a82d8eec2573122e629a4d197a52d71c
Iris Chen <o0lazybear0o@gmail.com>
2015-06-09 23:54:43 +0800

    test

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: Iris Chen
+    > Mail: o0lazybear0o@gmail.com 
+    > Created Time: Tue Jun  9 23:50:36 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	return 0;
+}
