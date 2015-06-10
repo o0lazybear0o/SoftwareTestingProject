69180733cca02dd09a9b1f665e20de8c69b3ca6f
Iris Chen <o0lazybear0o@gmail.com>
2015-06-10 16:15:07 +0800

    test.cpp added

---
+++ test.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: Iris Chen
+    > Mail: o0lazybear0o@gmail.com 
+    > Created Time: Wed Jun 10 16:14:54 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
